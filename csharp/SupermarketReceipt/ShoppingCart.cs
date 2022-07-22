c6bf9bb881772e258d388b95ca799bcc5eca1914
Nicholas Lemay <lemaynicholas@gmail.com>
2022-07-22 10:30:06 -0400

    ExtractCalculteDiscount into its own method

--- csharp/SupermarketReceipt/ShoppingCart.cs
+++ csharp/SupermarketReceipt/ShoppingCart.cs
@@ -20,51 +20,59 @@ namespace SupermarketReceipt
 
         public void HandleOffers(Receipt receipt, Dictionary<Product, Offer> offers, SupermarketCatalog catalog)
         {
+            
             foreach (var product in UniqueItemsInCart())
             {
                 var quantity = TotalQuantityForProduct(product);
                 var quantityAsInt = (int) quantity;
-                if (offers.ContainsKey(product))
-                {
-                    var offer = offers[product];
-                    var unitPrice = catalog.GetUnitPrice(product);
-                    Discount discount = null;
-                    var x = 1;
-                    if (offer.OfferType == SpecialOfferType.ThreeForTwo)
-                    {
-                        x = 3;
-                    }
-                    else if (offer.OfferType == SpecialOfferType.TwoForAmount)
-                    {
-                        x = 2;
-                        if (quantityAsInt >= 2)
-                        {
-                            var total = offer.Argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice;
-                            var discountN = unitPrice * quantity - total;
-                            discount = new Discount(product, "2 for " + offer.Argument, -discountN);
-                        }
-                    }
-
-                    if (offer.OfferType == SpecialOfferType.FiveForAmount) x = 5;
-                    var numberOfXs = quantityAsInt / x;
-                    if (offer.OfferType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
-                    {
-                        var discountAmount = quantity * unitPrice - (numberOfXs * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
-                        discount = new Discount(product, "3 for 2", -discountAmount);
-                    }
-
-                    if (offer.OfferType == SpecialOfferType.TenPercentDiscount) discount = new Discount(product, offer.Argument + "% off", -quantity * unitPrice * offer.Argument / 100.0);
-                    if (offer.OfferType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5)
-                    {
-                        var discountTotal = unitPrice * quantity - (offer.Argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-                        discount = new Discount(product, x + " for " + offer.Argument, -discountTotal);
-                    }
-
-                    if (discount != null)
-                        receipt.AddDiscount(discount);
-                }
+                Discount discount = null;
+                discount = CalculateDiscountFor(offers, catalog, product, quantityAsInt, quantity, discount);
+                if (discount != null)
+                    receipt.AddDiscount(discount);
                 
             }
+        }
+
+        private static Discount CalculateDiscountFor(Dictionary<Product, Offer> offers, SupermarketCatalog catalog, Product product, int quantityAsInt, double quantity, Discount discount)
+        {
+            if (offers.ContainsKey(product))
+            {
+                var offer = offers[product];
+                var unitPrice = catalog.GetUnitPrice(product);
+
+                var x = 1;
+                if (offer.OfferType == SpecialOfferType.ThreeForTwo)
+                {
+                    x = 3;
+                }
+                else if (offer.OfferType == SpecialOfferType.TwoForAmount)
+                {
+                    x = 2;
+                    if (quantityAsInt >= 2)
+                    {
+                        var total = offer.Argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice;
+                        var discountN = unitPrice * quantity - total;
+                        discount = new Discount(product, "2 for " + offer.Argument, -discountN);
+                    }
+                }
+
+                if (offer.OfferType == SpecialOfferType.FiveForAmount) x = 5;
+                var numberOfXs = quantityAsInt / x;
+                if (offer.OfferType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
+                {
+                    var discountAmount = quantity * unitPrice - (numberOfXs * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
+                    discount = new Discount(product, "3 for 2", -discountAmount);
+                }
+
+                if (offer.OfferType == SpecialOfferType.TenPercentDiscount) discount = new Discount(product, offer.Argument + "% off", -quantity * unitPrice * offer.Argument / 100.0);
+                if (offer.OfferType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5)
+                {
+                    var discountTotal = unitPrice * quantity - (offer.Argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+                    discount = new Discount(product, x + " for " + offer.Argument, -discountTotal);
+                }
+            }
+
+            return discount;
         }
 
         private IEnumerable<Product> UniqueItemsInCart()

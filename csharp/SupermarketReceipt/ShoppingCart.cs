d0054aad7230078b97841863e2d1200ea86d6f3f
Nicholas Lemay <lemaynicholas@gmail.com>
2022-07-22 10:41:19 -0400

    Improve readability with guard clause

--- csharp/SupermarketReceipt/ShoppingCart.cs
+++ csharp/SupermarketReceipt/ShoppingCart.cs
@@ -24,44 +24,44 @@ namespace SupermarketReceipt
 
         private Discount CalculateDiscountFor(Product product, Dictionary<Product, Offer> offers, SupermarketCatalog catalog)
         {
+            if (!offers.ContainsKey(product)) return null;
+            
             Discount discount = null;
             var quantity = TotalQuantityForProduct(product);
             var quantityAsInt = (int) quantity;
-            if (offers.ContainsKey(product))
+
+            var offer = offers[product];
+            var unitPrice = catalog.GetUnitPrice(product);
+
+            var x = 1;
+            if (offer.OfferType == SpecialOfferType.ThreeForTwo)
             {
-                var offer = offers[product];
-                var unitPrice = catalog.GetUnitPrice(product);
+                x = 3;
+            }
+            else if (offer.OfferType == SpecialOfferType.TwoForAmount)
+            {
+                x = 2;
+                if (quantityAsInt >= 2)
+                {
+                    var total = offer.Argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice;
+                    var discountN = unitPrice * quantity - total;
+                    discount = new Discount(product, "2 for " + offer.Argument, -discountN);
+                }
+            }
 
-                var x = 1;
-                if (offer.OfferType == SpecialOfferType.ThreeForTwo)
-                {
-                    x = 3;
-                }
-                else if (offer.OfferType == SpecialOfferType.TwoForAmount)
-                {
-                    x = 2;
-                    if (quantityAsInt >= 2)
-                    {
-                        var total = offer.Argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice;
-                        var discountN = unitPrice * quantity - total;
-                        discount = new Discount(product, "2 for " + offer.Argument, -discountN);
-                    }
-                }
+            if (offer.OfferType == SpecialOfferType.FiveForAmount) x = 5;
+            var numberOfXs = quantityAsInt / x;
+            if (offer.OfferType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
+            {
+                var discountAmount = quantity * unitPrice - (numberOfXs * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
+                discount = new Discount(product, "3 for 2", -discountAmount);
+            }
 
-                if (offer.OfferType == SpecialOfferType.FiveForAmount) x = 5;
-                var numberOfXs = quantityAsInt / x;
-                if (offer.OfferType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
-                {
-                    var discountAmount = quantity * unitPrice - (numberOfXs * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
-                    discount = new Discount(product, "3 for 2", -discountAmount);
-                }
-
-                if (offer.OfferType == SpecialOfferType.TenPercentDiscount) discount = new Discount(product, offer.Argument + "% off", -quantity * unitPrice * offer.Argument / 100.0);
-                if (offer.OfferType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5)
-                {
-                    var discountTotal = unitPrice * quantity - (offer.Argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-                    discount = new Discount(product, x + " for " + offer.Argument, -discountTotal);
-                }
+            if (offer.OfferType == SpecialOfferType.TenPercentDiscount) discount = new Discount(product, offer.Argument + "% off", -quantity * unitPrice * offer.Argument / 100.0);
+            if (offer.OfferType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5)
+            {
+                var discountTotal = unitPrice * quantity - (offer.Argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+                discount = new Discount(product, x + " for " + offer.Argument, -discountTotal);
             }
 
             return discount;

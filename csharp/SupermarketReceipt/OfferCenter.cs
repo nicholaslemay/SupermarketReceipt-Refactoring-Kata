b23d7a66a36e3b98f9bcb8797208e54fddee31c9
Nicholas Lemay <lemaynicholas@gmail.com>
2022-07-23 12:04:55 -0400

    Catalog as constructor arg

--- csharp/SupermarketReceipt/OfferCenter.cs
+++ csharp/SupermarketReceipt/OfferCenter.cs
@@ -8,23 +8,25 @@ public class OfferCenter
 {
     private readonly ShoppingCart _shoppingCart;
     private Dictionary<Product, IOffer> _offers;
+    private readonly SupermarketCatalog _catalog;
 
-    public OfferCenter(ShoppingCart shoppingCart, Dictionary<Product, IOffer> offers)
+    public OfferCenter(ShoppingCart shoppingCart, Dictionary<Product, IOffer> offers, SupermarketCatalog catalog)
     {
         _shoppingCart = shoppingCart;
         _offers = offers;
+        _catalog = catalog;
     }
 
-    public IEnumerable<Discount> AllAvailableDiscountsBasedOn(Dictionary<Product, IOffer> offers, SupermarketCatalog catalog) =>
+    public IEnumerable<Discount> AllAvailableDiscountsBasedOn() =>
         _shoppingCart.UniqueItemsInCart()
-            .Select(p => CalculateDiscountFor(p,catalog))
+            .Select(p => CalculateDiscountFor(p))
             .Where(d=> d !=null);
 
-    private Discount CalculateDiscountFor(Product product,  SupermarketCatalog catalog)
+    private Discount CalculateDiscountFor(Product product)
     {
         if (!_offers.ContainsKey(product)) return null;
 
-        var unitPrice = catalog.GetUnitPrice(product);
+        var unitPrice = _catalog.GetUnitPrice(product);
         var offer = _offers[product];
 
         return offer.CalculateDiscount(unitPrice, _shoppingCart.TotalQuantityForProduct(product));

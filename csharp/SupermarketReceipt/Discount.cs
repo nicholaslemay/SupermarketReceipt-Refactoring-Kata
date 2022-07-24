17980f1c3877617aaea0652cbc34baefda92680a
Nicholas Lemay <lemaynicholas@gmail.com>
2022-07-24 09:01:16 -0400

    Discount manages its own description

--- csharp/SupermarketReceipt/Discount.cs
+++ csharp/SupermarketReceipt/Discount.cs
@@ -2,14 +2,16 @@ namespace SupermarketReceipt
 {
     public class Discount
     {
+        private string _description;
+
         public Discount(Product product, string description, double discountAmount)
         {
+            _description = description;
             Product = product;
-            Description = description;
             DiscountAmount = discountAmount;
         }
 
-        public string Description { get; }
+        public string Description => $"{_description}({Product.Name})"; 
         public double DiscountAmount { get; }
         public Product Product { get; }
     }
